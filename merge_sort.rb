864c5b9f1ee8735ded80e3238a4881547cea5293
Catherine Kwak <khopsickle@users.noreply.github.com>
2016-11-10 10:48:49 -0700

    Start merge sort

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,32 +1,31 @@
-def merge_sort
+def merge_sort(arr)
+  return arr if arr.length == 1
 
+  midpoint = (arr.length - 1) / 2
+  left = arr[0..midpoint]
+  right = arr[midpoint..-1]
+
+  merge(merge_sort(left), merge_sort(right))
 end
 
+def merge(left, right)
+  merged_arr = []
 
+  until left.empty? && right.empty?
+    left_value = left[0]
+    right_value = right[0]
+    if right.empty? || left[0] < right[0]
+      merged_arr << left_value
+      left = left[1..-1]
+      left_value = left[0]
+    elsif left.empty? || left[0] > right[0]
+      merged_arr << right_value
+      right = right[1..-1]
+      right_value = right[0]
+    end
+  end
 
+  merged_arr
+end
 
-function mergeSort(array) {
-  // if the array is one element long, just return it
-
-  // mergeSort() the left half of the array
-
-  // mergeSort() the right half of the array
-
-  // merge() the two halves
-
-  // return the merged array
-}
-
-function merge(leftArr, rightArr){
-
-  // var newArr = [];
-
-  // compare leftArr[0] and rightArr[0]
-  // whichever is smaller, push it onto newArr
-
-  // repeat the process, but don't reuse elements,
-  // until you have pushed all the elements from
-  // leftArr and rightArr onto newArr in sorted order
-
-  // return newArr
-}
+p merge_sort([4, 1, 8, 11, 3, 2, 10])
